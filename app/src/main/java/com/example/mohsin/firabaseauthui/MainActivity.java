ab34e944e26beb9a54063a604e653ed9de2f88f8
Mohsin <mohsinhussain1993@gmail.com>
2018-06-30 20:10:42 +0500

    user authentication added with phone and email

--- app/src/main/java/com/example/mohsin/firabaseauthui/MainActivity.java
+++ app/src/main/java/com/example/mohsin/firabaseauthui/MainActivity.java
@@ -3,11 +3,29 @@ package com.example.mohsin.firabaseauthui;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.firebase.ui.auth.AuthUI;
+import com.google.firebase.auth.FirebaseAuth;
+
+import java.util.Arrays;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        FirebaseAuth auth = FirebaseAuth.getInstance();
+        startActivityForResult(
+                AuthUI.getInstance()
+                        .createSignInIntentBuilder()
+                        .setAvailableProviders(Arrays.asList(
+                                new AuthUI.IdpConfig.EmailBuilder().build(),
+                                //new AuthUI.IdpConfig.GoogleBuilder().build(),
+                                new AuthUI.IdpConfig.PhoneBuilder().build()))
+                        .build(),
+                1);
+
+
     }
 }
